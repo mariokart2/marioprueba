ba6cbcdbb740862b240b16fd81307233bcb60faa
mariokart2 <66957828+mariokart2@users.noreply.github.com>
2020-06-19 14:34:59 -0500

    Add files via upload

---
+++ mariorogelio.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int metros, litros;
+
+cout<<"ingresa la cantidad de metros cuadrados para convertirse a litros"<<endl;
+cin>>metros;
+litros=metros*1000;	
+cout<<metros<<" metros cuadrados equivale a "<<litros<<" litros"<<endl;	
+	
+	
+	
+	return 0;
+	
+	
+	
+	
+}
