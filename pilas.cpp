6d39300671d7acc212c89aedc12251d3048b6935
mariokart2 <66957828+mariokart2@users.noreply.github.com>
2020-07-05 16:12:28 -0500

    Add files via upload

--- pilas.cpp
+++ pilas.cpp
@@ -9,37 +9,47 @@ int dato;
 N *siguiente;	
 	
 };
+void sumaPila(N *&, int);  //prototipo de funcion
+void sacarpila(N *&, int &);  //prototipo de funcion
 
-
-
-
-void sumaPila(N *&, int);
-
-
-
-int main()
-{
+int main(){
 	N *pila=NULL; 
-	int n1, n2;
+	int dato;
 	
 	cout<<"ingrese un numero a la pila: "<<endl;
-	cin>>n1;
-	sumaPila(pila, n1);
+	cin>>dato;
+	sumaPila(pila, dato); //agregamos los datos a la pila
 	system("pause");
 	system("cls");
 		cout<<"ingrese otro numero: "<<endl;
-	cin>>n2;
-		sumaPila(pila, n2);
-	
+	cin>>dato;
+		sumaPila(pila, dato);
+		
+	cout<<"\nSacando los elementos de la pila: ";
+	while(pila != NULL){//mientras no sea el final de la fila
+		sacarpila(pila,dato);
+		if (pila != NULL){
+    cout<<dato<<" , "; //mientras pila sea diferente de NULL agregara "," para continuar con los datos
+		} 
+		else {
+			cout<<dato<<"."; // y cuando no pondra un punto final marcando el fin
+		}
+	}
 	getch();
 	return 0;
 }
- void sumaPila(N *&pila, int num)
- {
- 	N *nwNodo = new N();
- 	nwNodo -> dato = num; 
- 	nwNodo -> siguiente = pila;
- 	pila=nwNodo;
+ void sumaPila(N *&pila, int num){
+ 	N *nwNodo = new N(); //creamos un espacio de memoria
+ 	nwNodo -> dato = num; //cargamos el valor dentro del nodo
+ 	nwNodo -> siguiente = pila;//cargamos el puntero pila dentro del nodo
+ 	pila=nwNodo;//asignamos el nuevo nodo a pila
  	
  	cout<<num<<" se agrego a la pila"<<endl;
  }
+ void sacarpila(N *&pila, int &n){
+	N *aux=pila;
+	n = aux->dato;
+	pila= aux->siguiente;
+	delete aux;
+}
+
